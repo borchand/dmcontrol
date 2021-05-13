5639d8027fa190b568f4682227861072418a39ab
Cam Allen <csal@brown.edu>
2021-05-13 01:31:32 -0400

    Update .sh scripts

--- launch-experiment.sh
+++ launch-experiment.sh
@@ -1,5 +1,5 @@
 exp_name=fixenc
-exp_number=4
+exp_num=4
 tasks=1-3
 onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_cartpole_${exp_num} --tasklist ${tasks}
 onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_ball_in_cup_${exp_num} --tasklist ${tasks}
