2c6efc95991f93ceaabed578c30ce0ca59cef6ff
Cam Allen <csal@brown.edu>
2021-05-13 02:40:30 -0400

    Switch to echo to fix quoted string problem

--- launch-experiment.sh
+++ launch-experiment.sh
@@ -2,9 +2,9 @@ exp_name=exp5_inv1.0_relu
 tasks=1-3
 venv="--venv ./venv"
 settings="--backend gridengine --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 ${venv}"
-onager launch ${settings} --jobname markov_${exp_name}_cartpole --tasklist ${tasks}
-onager launch ${settings} --jobname markov_${exp_name}_ball_in_cup --tasklist ${tasks}
-onager launch ${settings} --jobname markov_${exp_name}_finger --tasklist ${tasks}
-onager launch ${settings} --jobname markov_${exp_name}_reacher --tasklist ${tasks}
-onager launch ${settings} --jobname markov_${exp_name}_walker --tasklist ${tasks}
-onager launch ${settings} --jobname markov_${exp_name}_cheetah --tasklist ${tasks}
+echo onager launch ${settings} --jobname markov_${exp_name}_cartpole --tasklist ${tasks}
+echo onager launch ${settings} --jobname markov_${exp_name}_ball_in_cup --tasklist ${tasks}
+echo onager launch ${settings} --jobname markov_${exp_name}_finger --tasklist ${tasks}
+echo onager launch ${settings} --jobname markov_${exp_name}_reacher --tasklist ${tasks}
+echo onager launch ${settings} --jobname markov_${exp_name}_walker --tasklist ${tasks}
+echo onager launch ${settings} --jobname markov_${exp_name}_cheetah --tasklist ${tasks}
--- prelaunch-experiment.sh
+++ prelaunch-experiment.sh
@@ -5,9 +5,10 @@ smoothness_dz=0.01
 # prefix='PIPENV_IGNORE_VIRTUALENVS=1 xvfb-run -a pipenv run '
 prefix="MUJOCO_GL=egl unbuffer xvfb-run -a "
 base_cmd="${prefix} python -m train --replicate --markov_inv_coef ${inv_coef} --markov_smoothness_coef ${smoothness_coef} --markov_smoothness_max_dz ${smoothness_dz} --work_dir ./tmp/${exp_name}"
-onager prelaunch +command '"'${base_cmd} --domain_name cheetah --task_name run --markov_lr 2e-4'"' +arg --seed {1..10} +jobname markov_${exp_name}_cheetah +tag --tag +tag-args '' +q
-onager prelaunch +command '"'${base_cmd} --domain_name cartpole --task_name swingup --markov_lr 1e-3'"' +arg --seed {1..10} +jobname markov_${exp_name}_cartpole +tag --tag +tag-args '' +q
-onager prelaunch +command '"'${base_cmd} --domain_name ball_in_cup --task_name catch --markov_lr 1e-3'"' +arg --seed {1..10} +jobname markov_${exp_name}_ball_in_cup +tag --tag +tag-args '' +q
-onager prelaunch +command '"'${base_cmd} --domain_name finger --task_name spin --markov_lr 1e-3'"' +arg --seed {1..10} +jobname markov_${exp_name}_finger +tag --tag +tag-args '' +q
-onager prelaunch +command '"'${base_cmd} --domain_name reacher --task_name easy --markov_lr 1e-3'"' +arg --seed {1..10} +jobname markov_${exp_name}_reacher +tag --tag +tag-args '' +q
-onager prelaunch +command '"'${base_cmd} --domain_name walker --task_name walk --markov_lr 1e-3'"' +arg --seed {1..10} +jobname markov_${exp_name}_walker +tag --tag +tag-args '' +q
+suffix="+tag --tag +tag-args '' +q"
+echo onager prelaunch +command \"${base_cmd} --domain_name cheetah --task_name run --markov_lr 2e-4 \" +arg --seed {1..10} +jobname markov_${exp_name}_cheetah ${suffix}
+echo onager prelaunch +command \"${base_cmd} --domain_name cartpole --task_name swingup --markov_lr 1e-3\" +arg --seed {1..10} +jobname markov_${exp_name}_cartpole ${suffix}
+echo onager prelaunch +command \"${base_cmd} --domain_name ball_in_cup --task_name catch --markov_lr 1e-3\" +arg --seed {1..10} +jobname markov_${exp_name}_ball_in_cup ${suffix}
+echo onager prelaunch +command \"${base_cmd} --domain_name finger --task_name spin --markov_lr 1e-3\" +arg --seed {1..10} +jobname markov_${exp_name}_finger ${suffix}
+echo onager prelaunch +command \"${base_cmd} --domain_name reacher --task_name easy --markov_lr 1e-3\" +arg --seed {1..10} +jobname markov_${exp_name}_reacher ${suffix}
+echo onager prelaunch +command \"${base_cmd} --domain_name walker --task_name walk --markov_lr 1e-3\" +arg --seed {1..10} +jobname markov_${exp_name}_walker ${suffix}
