716bddafeeee5bd70af03ca6929518a6d785459b
Cam Allen <csal@brown.edu>
2021-05-13 01:35:18 -0400

    Remove exp_num arg from .sh scripts

--- launch-experiment.sh
+++ launch-experiment.sh
@@ -1,9 +1,8 @@
-exp_name=fixenc
-exp_num=4
+exp_name=exp5_inv1.0_relu
 tasks=1-3
-onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_cartpole_${exp_num} --tasklist ${tasks}
-onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_ball_in_cup_${exp_num} --tasklist ${tasks}
-onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_finger_${exp_num} --tasklist ${tasks}
-onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_reacher_${exp_num} --tasklist ${tasks}
-onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_walker_${exp_num} --tasklist ${tasks}
-onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_cheetah_${exp_num} --tasklist ${tasks}
+onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_cartpole --tasklist ${tasks}
+onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_ball_in_cup --tasklist ${tasks}
+onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_finger --tasklist ${tasks}
+onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_reacher --tasklist ${tasks}
+onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_walker --tasklist ${tasks}
+onager launch --backend slurm --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 --jobname markov_${exp_name}_cheetah --tasklist ${tasks}
