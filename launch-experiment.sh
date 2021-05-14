4c23dcc446c3e09d8abe2e572e91f8cb726cc850
Cam Allen <csal@brown.edu>
2021-05-14 00:47:59 -0400

    Fix shell scripts and add better arg checking

--- launch-experiment.sh
+++ launch-experiment.sh
@@ -1,10 +1,10 @@
-exp_name=exp5_inv1.0_relu
-tasks=1-3
+if [ -z ${exp_name+x} ]; then echo "exp_name is unset. try exp_name=exp0_default"; exit 1; fi
+if [ -z ${tasks+x} ]; then echo "tasks is unset. try tasks=1-3"; exit 1; fi
 venv="--venv ./venv"
 settings="--backend gridengine --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 ${venv}"
-echo onager launch ${settings} --jobname markov_${exp_name}_cartpole --tasklist ${tasks}
-echo onager launch ${settings} --jobname markov_${exp_name}_ball_in_cup --tasklist ${tasks}
-echo onager launch ${settings} --jobname markov_${exp_name}_finger --tasklist ${tasks}
-echo onager launch ${settings} --jobname markov_${exp_name}_reacher --tasklist ${tasks}
-echo onager launch ${settings} --jobname markov_${exp_name}_walker --tasklist ${tasks}
-echo onager launch ${settings} --jobname markov_${exp_name}_cheetah --tasklist ${tasks}
+`echo onager launch ${settings} --jobname markov_${exp_name}_cartpole --tasklist ${tasks}`
+`echo onager launch ${settings} --jobname markov_${exp_name}_ball_in_cup --tasklist ${tasks}`
+`echo onager launch ${settings} --jobname markov_${exp_name}_finger --tasklist ${tasks}`
+`echo onager launch ${settings} --jobname markov_${exp_name}_reacher --tasklist ${tasks}`
+`echo onager launch ${settings} --jobname markov_${exp_name}_walker --tasklist ${tasks}`
+`echo onager launch ${settings} --jobname markov_${exp_name}_cheetah --tasklist ${tasks}`
