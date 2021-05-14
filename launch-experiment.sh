8ebe328a9aa5cea37b7cf54b8612ba104fe2603c
Cam Allen <csal@brown.edu>
2021-05-14 01:03:47 -0400

    Fix backend error in shell scripts

--- launch-experiment.sh
+++ launch-experiment.sh
@@ -3,14 +3,14 @@ if [ -z ${tasks+x} ]; then echo "tasks is unset. try tasks=1-3"; exit 1; fi
 venv="--venv ./venv"
 if command -v sbatch &> /dev/null;
 then
-    backend=slurm
+    backend="slurm"
 elif command -v qsub &> /dev/null;
 then
-    backend=gridengine
+    backend="gridengine"
 else
     echo "no backend detected"; exit 1
 fi
-settings="--backend ${backend} gridengine --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 ${venv}"
+settings="--backend ${backend} --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 ${venv}"
 `echo onager launch ${settings} --jobname markov_${exp_name}_cartpole --tasklist ${tasks}`
 `echo onager launch ${settings} --jobname markov_${exp_name}_ball_in_cup --tasklist ${tasks}`
 `echo onager launch ${settings} --jobname markov_${exp_name}_finger --tasklist ${tasks}`
