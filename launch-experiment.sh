5bda42588ddca9500d151cba04e7099967d8cbf3
Cam Allen <csal@brown.edu>
2021-05-14 00:53:32 -0400

    Auto-detect backend

--- launch-experiment.sh
+++ launch-experiment.sh
@@ -1,7 +1,15 @@
 if [ -z ${exp_name+x} ]; then echo "exp_name is unset. try exp_name=exp0_default"; exit 1; fi
 if [ -z ${tasks+x} ]; then echo "tasks is unset. try tasks=1-3"; exit 1; fi
 venv="--venv ./venv"
-settings="--backend gridengine --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 ${venv}"
+if command -v sbatch &> /dev/null
+then
+    backend=slurm
+elif command -v qsub &> /dev/null
+    backend=gridengine
+else
+    echo "no backend detected"; exit 1
+fi
+settings="--backend ${backend} gridengine --mem 48 --gpus 1 --cpus 1 --duration 2-12:00:00 ${venv}"
 `echo onager launch ${settings} --jobname markov_${exp_name}_cartpole --tasklist ${tasks}`
 `echo onager launch ${settings} --jobname markov_${exp_name}_ball_in_cup --tasklist ${tasks}`
 `echo onager launch ${settings} --jobname markov_${exp_name}_finger --tasklist ${tasks}`
--- prelaunch-experiment.sh
+++ prelaunch-experiment.sh
@@ -2,8 +2,16 @@ if [ -z ${exp_name+x} ]; then echo "exp_name is unset. try exp_name=exp0_default
 inv_coef=1.0
 smoothness_coef=0.0
 smoothness_dz=0.01
-prefix='PIPENV_IGNORE_VIRTUALENVS=1 xvfb-run -a pipenv run '
-# prefix="MUJOCO_GL=egl unbuffer xvfb-run -a "
+if command -v sbatch &> /dev/null
+then
+    # backend=slurm
+    prefix='PIPENV_IGNORE_VIRTUALENVS=1 xvfb-run -a pipenv run '
+elif command -v qsub &> /dev/null
+    # backend=gridengine
+    prefix="MUJOCO_GL=egl unbuffer xvfb-run -a "
+else
+    echo "no backend detected"; exit 1
+fi
 base_cmd="${prefix} python -m train --replicate --markov --markov_inv_coef ${inv_coef} --markov_smoothness_coef ${smoothness_coef} --markov_smoothness_max_dz ${smoothness_dz} --work_dir ./tmp/${exp_name}"
 suffix="+tag --tag +tag-args '' +q"
 `echo onager prelaunch +command \"${base_cmd} --domain_name cheetah --task_name run --markov_lr 2e-4 \" +arg --seed {1..10} +jobname markov_${exp_name}_cheetah ${suffix}`
